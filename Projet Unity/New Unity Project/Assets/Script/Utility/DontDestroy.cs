023c9705d579840bd8084cee638d8674066fa427
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2018-05-11 20:35:58 +0200

    Mise en place unity
    Projet unity + changements scene + script singleton

---
+++ Projet Unity/New Unity Project/Assets/Script/Utility/DontDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+	public bool Dont_Destroy;
+	// Use this for initialization
+	void Start () {
+		if (Dont_Destroy)
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+}
