1ec5610739a5cbd2e547fa509fd966105c5f2604
TheTitanKiller <38284142+TheTitanKiller@users.noreply.github.com>
2018-05-12 08:30:01 +0200

    Merge branch 'master' of https://github.com/m21-cerutti/GameJamePLUSLink
    Player Controller added.

---
+++ Projet Unity/New Unity Project (2)/Assets/Script/Character/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Rigidbody2D player;
+
+	public float speed = 1f;
+
+	private float vertical;
+	private float horizontal;
+	
+	void Start ()
+	{
+		player = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		vertical = Input.GetAxis("Vertical");
+		horizontal = Input.GetAxis("Horizontal");
+	}
+
+	void FixedUpdate ()
+	{
+		player.velocity = new Vector2(horizontal, vertical) * speed;
+	}
+}
