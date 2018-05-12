33a49d2ae3b264d33caedc54d753d4700a52a4bc
TheTitanKiller <38284142+TheTitanKiller@users.noreply.github.com>
2018-05-12 10:58:24 +0200

    Player and bullets
    Added PlayerShoot.cs and a bullet prefab + script.

---
+++ Projet Unity/New Unity Project (2)/Assets/Script/Character/PlayerShoot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+	public GameObject bullet;
+
+	private bool shoot;
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		shoot = Input.GetMouseButtonDown(0);
+	}
+
+	void FixedUpdate()
+	{
+		if(shoot)
+			Shoot();
+	}
+
+	void Shoot()
+	{
+		Instantiate(bullet, transform.position, Quaternion.LookRotation(Input.mousePosition));
+		Debug.Log("Shoot !");
+	}
+}
