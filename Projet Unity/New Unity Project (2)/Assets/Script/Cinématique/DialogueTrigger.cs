86cfc421dbc9b993c8cfb0ce8d9f8d76c51f5dc4
TheTitanKiller <38284142+TheTitanKiller@users.noreply.github.com>
2018-05-12 16:25:17 +0200

    Bullets done.

--- Projet Unity/New Unity Project (2)/Assets/Script/Cinématique/DialogueTrigger.cs
+++ Projet Unity/New Unity Project (2)/Assets/Script/Cinématique/DialogueTrigger.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour {
+public class DialogueTrigger : MonoBehaviour
+{
+	public Dialogue dialogue;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void dialogueTrigger()
+	{
+		FindObjectOfType<TextDisplay>().startDialogue(dialogue);
 	}
 }
