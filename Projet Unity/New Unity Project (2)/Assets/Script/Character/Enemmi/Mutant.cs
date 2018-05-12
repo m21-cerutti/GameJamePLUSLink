201c763d81a7542986866ff369d540056e514f27
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2018-05-12 11:15:45 +0200

    unlocked souris

--- Projet Unity/New Unity Project (2)/Assets/Script/Character/Enemmi/Mutant.cs
+++ Projet Unity/New Unity Project (2)/Assets/Script/Character/Enemmi/Mutant.cs
@@ -11,6 +11,13 @@ public class Mutant : CharacterObj {
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
+		foreach (ContactPoint2D pt in coll.contacts)
+		{
+			if (pt.otherCollider.name == "Player")
+			{
+				pt.otherCollider.SendMessage("takeDamage",damage);
+			}
+		}
 		
 	}
 
