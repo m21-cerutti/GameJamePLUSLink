f462c697347d1c1f96a1779def055a3ae927e83e
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2018-05-13 11:11:52 +0200

    Améliorations feed back

--- Projet Unity/New Unity Project (2)/Assets/Script/Character/Player/PlayerMotor.cs
+++ Projet Unity/New Unity Project (2)/Assets/Script/Character/Player/PlayerMotor.cs
@@ -7,10 +7,12 @@ public class PlayerMotor : CharacterObj {
 	public float invulnerability;
 	float timeinv;
 
+	SpriteRenderer rend;
+
 	public new void takeDamage(int dam)
 	{
 		//Debug.Log(this.gameObject.name + " " + life);
-		if (timeinv < 0 )
+		if (timeinv <= 0 )
 		{
 			base.takeDamage(dam);
 			timeinv = invulnerability;
@@ -23,14 +25,22 @@ public class PlayerMotor : CharacterObj {
 	}
 
 	void Start () {
-		
+		rend = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (invulnerability > 0)
+		if (timeinv > 0)
 		{
+			rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, 0.5f);
 			timeinv -= Time.deltaTime;
+		}
+		else
+		{
+			if (rend.color.a < 0.9f)
+			{
+				rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, 1);
+			}
 		}
 	}
 }
--- Projet Unity/New Unity Project (2)/Assets/Script/Musique fond/MusicManager.cs
+++ Projet Unity/New Unity Project (2)/Assets/Script/Musique fond/MusicManager.cs
@@ -2,28 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MusicManager :  SingletonBehaviour<MusicManager>
 {
 
-	public GameObject audioFond;
-	GameObject Audio;
+	public GameObject audioFondPrefab;
+	public GameObject audioNoisePrefab;
+	AudioSource audioFond;
+	AudioSource audioNoise;
 	public bool win;
+
+	public AudioClip MainMenu;
+	public AudioClip Game;
+
+	[SerializeField]
+	public Noise[] Noises;
+
+	bool priority = false;
+
+	[System.Serializable]
+	public struct Noise
+	{
+		public AudioClip clip;
+		public bool important;
+	};
+
+	public void playNoise(int nb)
+	{
+		if (audioNoise !=null && (!audioNoise.isPlaying || (priority == false && Noises[nb].important)))
+		{
+			audioNoise.clip = Noises[nb].clip;
+			audioNoise.Play();
+			priority = Noises[nb].important;
+		}
+	}
 
 	new void Awake ()
 	{
 		base.Awake();
 		if (!GameObject.Find ("AudioBackground(Clone)")) {
-			Audio= Instantiate(audioFond);
-			Audio.transform.SetParent(GameObject.FindGameObjectWithTag("MainCamera").transform);
-		}	
+			audioFond = Instantiate(audioFondPrefab).GetComponent<AudioSource>();
+			audioFond.transform.SetParent(GameObject.FindGameObjectWithTag("MainCamera").transform);
+			audioFond.clip = MainMenu;
+		}
+		if (!GameObject.Find("AudioNoise(Clone)"))
+		{
+			audioNoise = Instantiate(audioNoisePrefab).GetComponent<AudioSource>();
+			audioNoise.transform.SetParent(GameObject.FindGameObjectWithTag("MainCamera").transform);
+		}
+	}
+
+	void Start()
+	{
+		audioFond.PlayDelayed(0.3f);
 	}
 
 	private void Update()
 	{
-		if (win && Audio != null)
+		
+		if (audioNoise == null)
 		{
-			Audio.GetComponent<AudioSource>().volume = Mathf.Lerp(Audio.GetComponent<AudioSource>().volume, 0f, 0.02f);
+			audioNoise = Instantiate(audioNoisePrefab).GetComponent<AudioSource>();
+			audioNoise.transform.SetParent(GameObject.FindGameObjectWithTag("MainCamera").transform);
+
+		}
+
+		if (audioFond == null)
+		{
+
+			audioFond = Instantiate(audioNoisePrefab).GetComponent<AudioSource>();
+			audioFond.transform.SetParent(GameObject.FindGameObjectWithTag("MainCamera").transform);
+
+		}
+		else
+		{
+			if (HUDManager.Instance.state == stateMenu.Main  && audioFond.clip != MainMenu)
+			{
+				audioFond.Stop();
+				audioFond.clip = MainMenu;
+				audioFond.PlayDelayed(0.3f);
+
+			}
+
+			if (HUDManager.Instance.state == stateMenu.Play  && audioFond.clip != Game)
+			{
+				audioFond.Stop();
+				audioFond.clip = Game;
+				audioFond.PlayDelayed(0.3f);
+			}
+
+			if (HUDManager.Instance.state == stateMenu.Win)
+			{
+				audioFond.volume = Mathf.Lerp(audioFond.volume, 0f, 0.02f);
+			}
 		}
 	}
 }
