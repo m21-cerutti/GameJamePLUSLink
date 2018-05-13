6445a092a2880db336bbf5a6577efef652507f7f
TheTitanKiller <38284142+TheTitanKiller@users.noreply.github.com>
2018-05-13 18:02:13 +0200

    Bulles + drop rockets

--- Projet Unity/New Unity Project (2)/Assets/Script/EndScript.cs
+++ Projet Unity/New Unity Project (2)/Assets/Script/EndScript.cs
@@ -6,7 +6,7 @@ public class EndScript : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		if (col.tag == "Player")
+		if (col.tag == "Player" && (PlayerMotor.nbEnemies <= 0))
 		{
 			HUDManager.Instance.Win();
 		}
