b2818bba19c1bd522cb6e35b33ca7c49df0181f4
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2018-05-13 14:55:07 +0200

    Bullet

--- Projet Unity/New Unity Project (2)/Assets/Script/Weapons/Bullet.cs
+++ Projet Unity/New Unity Project (2)/Assets/Script/Weapons/Bullet.cs
@@ -1,39 +1,86 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+
+	public int damage;
+	public bool isRocket;
+	public float radius;
+	public GameObject Explosion;
+	private object yield;
+	Rigidbody2D rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+
+		if (Explosion != null)
+		{
+			Explosion.transform.localScale = Explosion.transform.localScale * radius;
+		}
+	}
+
+
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		foreach (ContactPoint2D con in other.contacts)
 		{
-			if (other.collider.tag == "Enemy")
+			if (isRocket)
+			{
+				explosiveDamage(other.collider, damage);
+				StartCoroutine(Explose(3f));
+				break;
+			}
+			else if (other.collider.tag == "Enemy") {
 				if (other.collider.GetComponent<Rigidbody2D>().bodyType != RigidbodyType2D.Static)
+				{
 					other.collider.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-				bulletDamage(other.collider, 2f);
+				}
+					bulletDamage(other.collider, damage);
+			}
+			Destroy(gameObject);
 		}
-		Destroy(gameObject);
+		
 
 	}
-	/*
+
 	private void OnBecameInvisible()
 	{
 		Destroy(gameObject);
 	}
-	*/
-	void bulletDamage(Collider2D other, float damage)
+
+	void bulletDamage(Collider2D other, int damage)
 	{
 		if(other.tag == "Enemy")
 			other.SendMessage("takeDamage", damage);
 	}
 
-	void explosiveDamage(Collider2D other, float radius, float damage)
+	void explosiveDamage(Collider2D other, int damage)
 	{
 		Collider2D[] colliders = new Collider2D[20];
-		Physics2D.OverlapCircleNonAlloc(transform.position, radius, colliders, (8 | 10));
+		LayerMask layerMask = 1 << 8 | 1 << 10;
+		Physics2D.OverlapCircleNonAlloc(transform.position, radius, colliders, layerMask);
+
 		foreach (Collider2D entity in colliders)
 		{
-			entity.SendMessage("takeDamage", damage);
-			Debug.Log(entity.name + " has taken damage !");
+			if (entity != null && (entity.tag == "Enemy" || entity.tag == "Player"))
+			{
+				Debug.Log(entity.name);
+				entity.SendMessage("takeDamage", damage);
+				Debug.Log(entity.name + " has taken damage !");
+			}
 		}
 	}
+
+	IEnumerator Explose(float time)
+	{
+		rb.simulated = false;
+		Explosion.SetActive(true);
+		yield return new WaitForSeconds(time);
+		Destroy(gameObject);
+	}
+
+
 }
